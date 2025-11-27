8da0163e39dc2c4fa09950c11be131c938057803
BANGWON LEE <qkddnjs31@gmail.com>
2025-11-28 08:58:14 +0900

    add : 루트를 생성하는 조건을  main 컴포넌트에서 작성

--- src/features/Route/components/RouteMain.tsx
+++ src/features/Route/components/RouteMain.tsx
@@ -1,13 +1,49 @@
+'use client'
+import { useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
 import RouteBottom from './RouteBottom'
 import RouteHeader from './RouteHeader'
 import RouteMap from './RouteMap'
+import RoutePlace from './RoutePlace'
+import { getMyRouteList } from '../containers/RouteMainContainer'
+import { getMapOptions, getMyLocAddress } from '@/util/map/mapFunctions'
 
 export default function RouteMain() {
+  const searchParams = useSearchParams()
+  const queryPurposes = searchParams?.get('purposes') ?? '' // ?text=카페 → "카페" (fallback to empty string if null)
+  const queryTime = searchParams?.get('time') ?? '' // ?text=카페 → "카페" (fallback to empty string if null)
+
+  async function getMyLoc(): Promise<{
+    jibunAddress: string
+    roadAddress: string
+  }> {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        async pos => {
+          const address = await getMyLocAddress(pos)
+          resolve(address)
+        },
+        err => reject(err)
+      )
+    })
+  }
+
+  useEffect(() => {
+    const getData = async () => {
+      console.log('render useEffect')
+      const myLoc = await getMyLoc()
+      getMyRouteList(myLoc.jibunAddress, queryPurposes, queryTime)
+    }
+
+    getData()
+  }, [])
+
   return (
     <div className="font-sans">
       <div className="min-h-screen bg-gray-50">
         <RouteHeader />
         <RouteMap />
+        <RoutePlace />
         <RouteBottom />
       </div>
     </div>
