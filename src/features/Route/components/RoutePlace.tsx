aefd3c9c262873334dec7e19b6a037880554580f
BANGWON LEE <qkddnjs31@gmail.com>
2025-12-04 08:47:47 +0900

    add : api로 받아온 데이터 뿌려주는 작업

--- src/features/Route/components/RoutePlace.tsx
+++ src/features/Route/components/RoutePlace.tsx
@@ -1,7 +1,13 @@
 import { StatLabel, StatValue } from '@/share/components/Text'
-import { Clock, MapPin, Star } from 'lucide-react'
+import { placeType, RoutePlaceType } from '@/types/RouteType'
+import { getHourTimeMinTimeFormat } from '@/util/common/common'
+import { Clock, MapPin, Phone } from 'lucide-react'
 
-export default function RoutePlace() {
+export default function RoutePlace(props: RoutePlaceType) {
+  const { routeArr } = props
+
+  console.log('route', routeArr)
+
   return (
     <div className="max-w-md mx-auto p-4 space-y-4 pb-24">
       <div className=" bg-white rounded-2xl p-4 shadow-lg">
@@ -16,6 +22,7 @@ export default function RoutePlace() {
           </div>
         </div>
       </div>
+      {/* 
       {[
         {
           id: 1,
@@ -41,13 +48,14 @@ export default function RoutePlace() {
           distance: '450m',
           time: '6분',
         },
-      ].map((place, index) => (
+      ] */}
+      {routeArr.map((place: placeType, index: number) => (
         <div
-          key={place.id}
+          key={index + 1}
           className="bg-white rounded-2xl overflow-hidden shadow-md hover:shadow-lg transition-all duration-300 cursor-pointer"
         >
           <div className="flex">
-            <div className="w-28 h-28 bg-gradient-to-br from-indigo-100 to-purple-100"></div>
+            {/* <div className="w-28 h-28 bg-gradient-to-br from-indigo-100 to-purple-100"></div> */}
             <div className="flex-1 p-4">
               <div className="flex items-start justify-between mb-2">
                 <div>
@@ -58,21 +66,33 @@ export default function RoutePlace() {
                     <h3 className="font-bold text-gray-900">{place.name}</h3>
                   </div>
                   {/* StatLabel 컴포넌트 사용 */}
-                  <StatLabel>{place.type}</StatLabel>
+                  <StatLabel>{place.middleBizName}</StatLabel>
                 </div>
               </div>
-              <div className="flex items-center gap-3 text-sm text-gray-600">
+              <div className="grid items-center gap-3 text-sm text-gray-600">
                 <div className="flex items-center gap-1">
-                  <Star className="w-4 h-4 text-amber-400 fill-amber-400" />
-                  <span>{place.rating}</span>
+                  <Phone className="w-4 h-4 text-amber-400 fill-amber-400" />
+                  <span>{place.telNo}</span>
                 </div>
                 <div className="flex items-center gap-1">
                   <MapPin className="w-4 h-4" />
-                  <span>{place.distance}</span>
+                  <span>
+                    {place.newAddressList.newAddress[0].fullAddressRoad}
+                  </span>
                 </div>
                 <div className="flex items-center gap-1">
                   <Clock className="w-4 h-4" />
-                  <span>{place.time}</span>
+                  <span>
+                    {getHourTimeMinTimeFormat(Number(place.radius)).hours > 0
+                      ? `${getHourTimeMinTimeFormat(
+                          Number(place.radius)
+                        ).hours.toString()}시간 ${getHourTimeMinTimeFormat(
+                          Number(place.radius)
+                        ).minutes.toString()}분`
+                      : `${getHourTimeMinTimeFormat(
+                          Number(place.radius)
+                        ).minutes.toString()}분`}{' '}
+                  </span>
                 </div>
               </div>
             </div>
