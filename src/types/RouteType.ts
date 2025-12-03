a2eb307fdd2e2c3ed55033877a75a098b3a27ebf
BANGWON LEE <qkddnjs31@gmail.com>
2025-12-04 08:48:04 +0900

    add : 타입 코드 생성

---
+++ src/types/RouteType.ts
@@ -0,0 +1,11 @@
+export type placeType = {
+  name: string
+  middleBizName: string
+  telNo: string
+  radius: string
+  newAddressList: string[]
+}
+
+export type RoutePlaceType = {
+  routeArr: Record<string, string[]>
+}
