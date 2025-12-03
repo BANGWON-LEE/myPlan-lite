fe988d8ca6134af4fa7bea6ae340c2113b65f42a
BANGWON LEE <qkddnjs31@gmail.com>
2025-12-04 08:48:32 +0900

    add : plan 및 route 개발 관련 함수 선언

--- src/util/common/common.ts
+++ src/util/common/common.ts
@@ -15,12 +15,61 @@ export function getHourTimeMinTimeFormat(duration: number): {
   hours: number
   minutes: number
 } {
-  const totalSeconds = Math.floor(duration / 1000)
+  const totalSeconds = Math.floor(duration * 800) //
   const hours = Math.floor(totalSeconds / 3600) // 1시간 = 3600초
-  const minutes = Math.floor((totalSeconds % 3600) / 60) // 남은 초에서 분 계산
+  const minutes = Math.ceil((totalSeconds % 3600) / 60) // 남은 초에서 분 계산
   return { hours: hours, minutes: minutes }
 }
 
 export function convertGetKm(meter: number): number {
   return Math.round(meter / 1000)
+}
+
+export function filterApiData(apiArr: Record<string, number>) {
+  const placeNameArr = apiArr.searchPoiInfo.pois.poi
+
+  const result = placeNameArr.filter((place: Record<string, string>) => {
+    return (
+      !place.name.includes('주차장') &&
+      !place.name.includes('정문') &&
+      !place.name.includes('후문')
+    )
+  })
+
+  return result
+}
+
+export function formatStringToArray(str: string) {
+  return str.split(',')
+}
+
+export function addValueByCategory(
+  setRouteList: React.Dispatch<React.SetStateAction<Record<string, string[]>>>,
+  purpose: string,
+  place: string[]
+) {
+  switch (purpose) {
+    case '음식점':
+      return setRouteList(prev => ({
+        ...prev,
+        meal: [...place],
+      }))
+    case '커피':
+      return setRouteList(prev => ({
+        ...prev,
+        coffee: [...place],
+      }))
+    case '공원':
+      return setRouteList(prev => ({
+        ...prev,
+        walk: [...place],
+      }))
+    case '쇼핑':
+      return setRouteList(prev => ({
+        ...prev,
+        shopping: [...place],
+      }))
+    default:
+      null
+  }
 }
