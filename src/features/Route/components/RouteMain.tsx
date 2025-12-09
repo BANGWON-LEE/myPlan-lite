374c016be045fd72c2d139d06e3ea55a79d1c2f1
BANGWON LEE <qkddnjs31@gmail.com>
2025-12-09 13:35:36 +0900

    edit : suspense 추가

--- src/features/Route/components/RouteMain.tsx
+++ src/features/Route/components/RouteMain.tsx
@@ -1,18 +1,12 @@
 'use client'
-import RouteBottom from './RouteBottom'
-import RouteHeader from './RouteHeader'
-import RouteMap from './RouteMap'
+
+import { Suspense } from 'react'
 import RoutePlace from './RoutePlace'
 
 export default function RouteMain() {
   return (
-    <div className="font-sans">
-      <div className="min-h-screen bg-gray-50">
-        <RouteHeader />
-        <RouteMap />
-        <RoutePlace />
-        <RouteBottom />
-      </div>
-    </div>
+    <Suspense fallback={<div>loading...</div>}>
+      <RoutePlace />
+    </Suspense>
   )
 }
