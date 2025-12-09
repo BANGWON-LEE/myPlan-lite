74566814128f2f735734e86c35f2de9dd106fa29
BANGWON LEE <qkddnjs31@gmail.com>
2025-12-09 11:51:53 +0900

    edit : useLayoutEffect를 사용하여 loading 화면 그려줌

--- src/features/Route/components/RoutePlace.tsx
+++ src/features/Route/components/RoutePlace.tsx
@@ -1,28 +1,88 @@
+'use client'
+
 import { StatLabel, StatValue } from '@/share/components/Text'
-import { placeType, RoutePlaceType } from '@/types/placeType'
-import { getHourTimeMinTimeFormat } from '@/util/common/common'
+import { placeType, TmapPoiItem } from '@/types/placeType'
+import {
+  addValueByCategory,
+  filterApiData,
+  formatStringToArray,
+  getHourTimeMinTimeFormat,
+} from '@/util/common/common'
+import { getCurrentPositionPromise } from '@/util/map/mapFunctions'
 import { Clock, MapPin, Phone } from 'lucide-react'
+import { useSearchParams } from 'next/navigation'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
+import { getMyRouteList } from '../containers/RouteMainContainer'
+import LoadingScreen from '@/features/loading/components/LoadingScreen'
 
-export default function RoutePlace(props: RoutePlaceType) {
-  const { routeArr } = props
+export default function RoutePlace() {
+  const searchParams = useSearchParams()
+  const queryPurposes = searchParams?.get('purposes') ?? '' // ?text=카페 → "카페" (fallback to empty string if null)
+  const queryTime = searchParams?.get('time') ?? '' // ?text=카페 → "카페" (fallback to empty string if null)
 
-  console.log('route', routeArr)
+  const [routeList, setRouteList] = useState<Record<string, TmapPoiItem[]>>({
+    meal: [],
+    coffee: [],
+    walk: [],
+    shopping: [],
+  })
+
+  const [loadingScreenAction, setLoadingSreenAction] = useState<boolean>(false)
+
+  useLayoutEffect(() => {
+    const loadingTime = 1000
+    setLoadingSreenAction(true)
+
+    const delayTime = setTimeout(() => {
+      setLoadingSreenAction(false)
+    }, loadingTime)
+
+    return () => clearTimeout(delayTime)
+  }, [])
+
+  useEffect(() => {
+    const getData = async () => {
+      const purposesArr = formatStringToArray(queryPurposes)
+      const position = await getCurrentPositionPromise()
+
+      purposesArr.forEach(async (purpose: string) => {
+        addValueByCategory(
+          setRouteList,
+          purpose,
+          filterApiData(await getMyRouteList(position, purpose, queryTime))
+        )
+      })
+    }
+
+    getData()
+  }, [queryPurposes, queryTime])
+
+  const routeArr = [
+    routeList.meal[0],
+    routeList.coffee[0],
+    routeList.walk[0],
+    routeList.shopping[0],
+  ].filter(Boolean) // undefined 제거
 
   return (
-    <div className="max-w-md mx-auto p-4 space-y-4 pb-24">
-      <div className=" bg-white rounded-2xl p-4 shadow-lg">
-        <div className="flex items-center justify-between">
-          <div>
-            <StatLabel>총 소요시간</StatLabel>
-            <StatValue>약 10분</StatValue>
+    <React.Fragment>
+      {loadingScreenAction ? (
+        <LoadingScreen />
+      ) : (
+        <div className="max-w-md mx-auto p-4 space-y-4 pb-24">
+          <div className=" bg-white rounded-2xl p-4 shadow-lg">
+            <div className="flex items-center justify-between">
+              <div>
+                <StatLabel>총 소요시간</StatLabel>
+                <StatValue>약 10분</StatValue>
+              </div>
+              <div className="text-right">
+                <StatLabel>총 거리</StatLabel>
+                <StatValue color="text-gray-900">1.3km</StatValue>
+              </div>
+            </div>
           </div>
-          <div className="text-right">
-            <StatLabel>총 거리</StatLabel>
-            <StatValue color="text-gray-900">1.3km</StatValue>
-          </div>
-        </div>
-      </div>
-      {/* 
+          {/* 
       {[
         {
           id: 1,
@@ -49,56 +109,61 @@ export default function RoutePlace(props: RoutePlaceType) {
           time: '6분',
         },
       ] */}
-      {routeArr.map((place: placeType, index: number) => (
-        <div
-          key={index + 1}
-          className="bg-white rounded-2xl overflow-hidden shadow-md hover:shadow-lg transition-all duration-300 cursor-pointer"
-        >
-          <div className="flex">
-            {/* <div className="w-28 h-28 bg-gradient-to-br from-indigo-100 to-purple-100"></div> */}
-            <div className="flex-1 p-4">
-              <div className="flex items-start justify-between mb-2">
-                <div>
-                  <div className="flex items-center gap-2 mb-1">
-                    <span className="text-sm font-semibold text-indigo-600">
-                      #{index + 1}
-                    </span>
-                    <h3 className="font-bold text-gray-900">{place.name}</h3>
+          {routeArr.map((place: placeType, index: number) => (
+            <div
+              key={index + 1}
+              className="bg-white rounded-2xl overflow-hidden shadow-md hover:shadow-lg transition-all duration-300 cursor-pointer"
+            >
+              <div className="flex">
+                {/* <div className="w-28 h-28 bg-gradient-to-br from-indigo-100 to-purple-100"></div> */}
+                <div className="flex-1 p-4">
+                  <div className="flex items-start justify-between mb-2">
+                    <div>
+                      <div className="flex items-center gap-2 mb-1">
+                        <span className="text-sm font-semibold text-indigo-600">
+                          #{index + 1}
+                        </span>
+                        <h3 className="font-bold text-gray-900">
+                          {place.name}
+                        </h3>
+                      </div>
+                      {/* StatLabel 컴포넌트 사용 */}
+                      <StatLabel>{place.middleBizName}</StatLabel>
+                    </div>
                   </div>
-                  {/* StatLabel 컴포넌트 사용 */}
-                  <StatLabel>{place.middleBizName}</StatLabel>
-                </div>
-              </div>
-              <div className="grid items-center gap-3 text-sm text-gray-600">
-                <div className="flex items-center gap-1">
-                  <Phone className="w-4 h-4 text-amber-400 fill-amber-400" />
-                  <span>{place.telNo}</span>
-                </div>
-                <div className="flex items-center gap-1">
-                  <MapPin className="w-4 h-4" />
-                  <span>
-                    {place.newAddressList.newAddress[0].fullAddressRoad}
-                  </span>
-                </div>
-                <div className="flex items-center gap-1">
-                  <Clock className="w-4 h-4" />
-                  <span>
-                    {getHourTimeMinTimeFormat(Number(place.radius)).hours > 0
-                      ? `${getHourTimeMinTimeFormat(
-                          Number(place.radius)
-                        ).hours.toString()}시간 ${getHourTimeMinTimeFormat(
-                          Number(place.radius)
-                        ).minutes.toString()}분`
-                      : `${getHourTimeMinTimeFormat(
-                          Number(place.radius)
-                        ).minutes.toString()}분`}{' '}
-                  </span>
+                  <div className="grid items-center gap-3 text-sm text-gray-600">
+                    <div className="flex items-center gap-1">
+                      <Phone className="w-4 h-4 text-amber-400 fill-amber-400" />
+                      <span>{place.telNo}</span>
+                    </div>
+                    <div className="flex items-center gap-1">
+                      <MapPin className="w-4 h-4" />
+                      <span>
+                        {place.newAddressList.newAddress[0].fullAddressRoad}
+                      </span>
+                    </div>
+                    <div className="flex items-center gap-1">
+                      <Clock className="w-4 h-4" />
+                      <span>
+                        {getHourTimeMinTimeFormat(Number(place.radius)).hours >
+                        0
+                          ? `${getHourTimeMinTimeFormat(
+                              Number(place.radius)
+                            ).hours.toString()}시간 ${getHourTimeMinTimeFormat(
+                              Number(place.radius)
+                            ).minutes.toString()}분`
+                          : `${getHourTimeMinTimeFormat(
+                              Number(place.radius)
+                            ).minutes.toString()}분`}{' '}
+                      </span>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
-      ))}
-    </div>
+      )}
+    </React.Fragment>
   )
 }
