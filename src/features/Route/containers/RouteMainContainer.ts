7ab723499a613880700a82b125bf9a96c6ae080a
BANGWON LEE <qkddnjs31@gmail.com>
2025-12-09 14:34:19 +0900

    Edit : axios 대신 fetch 사용

--- src/features/Route/containers/RouteMainContainer.ts
+++ src/features/Route/containers/RouteMainContainer.ts
@@ -1,18 +1,26 @@
-import axios from 'axios'
-
 export async function getMyRouteList(
   position: GeolocationPosition,
   queryPurposes: string,
   queryTime: string
 ) {
-  const latitude = decodeURIComponent(position.coords.latitude.toString())
-  const longitude = decodeURIComponent(position.coords.longitude.toString())
-  const purpose = queryPurposes
-  const time = decodeURIComponent(queryTime)
-  console.log('purpose', purpose)
+  const latitude = position.coords.latitude
+  const longitude = position.coords.longitude
 
-  const res = await axios.get(
-    `/api/searchLoc?latitude=${latitude}&longitude=${longitude}&purpose=${purpose}&time=${time}`
-  )
-  return res.data
+  console.log('queryPurposes', queryPurposes)
+  const params = new URLSearchParams({
+    latitude: latitude.toString(),
+    longitude: longitude.toString(),
+    purpose: queryPurposes, // "커피" 그대로 넣어도 fetch가 자동 인코딩
+    time: queryTime,
+  })
+
+  const res = await fetch(`/api/searchLoc?${params.toString()}`, {
+    method: 'GET',
+  })
+
+  if (!res.ok) {
+    throw new Error('API error')
+  }
+
+  return res.json()
 }
