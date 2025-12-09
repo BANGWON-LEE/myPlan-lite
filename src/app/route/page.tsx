e33257d139c1d2c8f0e31470341414cdd1ed7810
BANGWON LEE <qkddnjs31@gmail.com>
2025-12-09 11:51:26 +0900

    edit : loadingScreen 사용을 위해 코드 수정

--- src/app/route/page.tsx
+++ src/app/route/page.tsx
@@ -1,13 +1,5 @@
-'use client'
-
-import LoadingScreen from '@/features/loading/components/LoadingScreen'
 import RouteMain from '@/features/Route/components/RouteMain'
-import { Suspense } from 'react'
 
 export default function RoutePage() {
-  return (
-    <Suspense fallback={<LoadingScreen />}>
-      <RouteMain />
-    </Suspense>
-  )
+  return <RouteMain />
 }
