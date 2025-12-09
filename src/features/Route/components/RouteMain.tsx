86c120a997c5adb1e2db6398b3f62b04f425fe9c
BANGWON LEE <qkddnjs31@gmail.com>
2025-12-09 15:37:19 +0900

    edit: suspense Fallback

--- src/features/Route/components/RouteMain.tsx
+++ src/features/Route/components/RouteMain.tsx
@@ -2,10 +2,11 @@
 
 import { Suspense } from 'react'
 import RoutePlace from './RoutePlace'
+import LoadingScreen from '@/features/loading/components/LoadingScreen'
 
 export default function RouteMain() {
   return (
-    <Suspense fallback={<div>loading...</div>}>
+    <Suspense fallback={<LoadingScreen />}>
       <RoutePlace />
     </Suspense>
   )
