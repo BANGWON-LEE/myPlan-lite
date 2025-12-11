701a7fcc05b5cdf5840f91b30eab2b8a5c4d2fe9
BANGWON LEE <qkddnjs31@gmail.com>
2025-12-11 23:18:57 +0900

    edit: Loading 처리 주석 (dynamic만 둠)

--- src/features/Route/components/RoutePlace.tsx
+++ src/features/Route/components/RoutePlace.tsx
@@ -17,11 +17,10 @@ const Icon = {
   MapPin: dynamic(() => import('lucide-react').then(m => m.MapPin)),
 }
 
-import { Clock, MapPin, Phone } from 'lucide-react'
 import { useSearchParams } from 'next/navigation'
 import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { getMyRouteList } from '../containers/RouteMainContainer'
-import LoadingScreen from '@/features/loading/components/LoadingScreen'
+// import LoadingScreen from '@/features/loading/components/LoadingScreen'
 
 export default function RoutePlace() {
   const searchParams = useSearchParams()
@@ -35,11 +34,11 @@ export default function RoutePlace() {
     shopping: [],
   })
 
-  const [loadingScreenAction, setLoadingSreenAction] = useState<boolean>(false)
+  // const [loadingScreenAction, setLoadingSreenAction] = useState<boolean>(false)
 
   useLayoutEffect(() => {
     const loadingTime = 1500
-    setLoadingSreenAction(true)
+    //     setLoadingSreenAction(true)
 
     const delayTime = setTimeout(() => {
       setLoadingSreenAction(false)
@@ -76,23 +75,23 @@ export default function RoutePlace() {
 
   return (
     <React.Fragment>
-      {loadingScreenAction ? (
+      {/* {loadingScreenAction ? (
         <LoadingScreen />
-      ) : (
-        <div className="max-w-md mx-auto p-4 space-y-4 pb-24">
-          <div className=" bg-white rounded-2xl p-4 shadow-lg">
-            <div className="flex items-center justify-between">
-              <div>
-                <StatLabel>총 소요시간</StatLabel>
-                <StatValue>약 10분</StatValue>
-              </div>
-              <div className="text-right">
-                <StatLabel>총 거리</StatLabel>
-                <StatValue color="text-gray-900">1.3km</StatValue>
-              </div>
+      ) : ( */}
+      <div className="max-w-md mx-auto p-4 space-y-4 pb-24">
+        <div className=" bg-white rounded-2xl p-4 shadow-lg">
+          <div className="flex items-center justify-between">
+            <div>
+              <StatLabel>총 소요시간</StatLabel>
+              <StatValue>약 10분</StatValue>
+            </div>
+            <div className="text-right">
+              <StatLabel>총 거리</StatLabel>
+              <StatValue color="text-gray-900">1.3km</StatValue>
             </div>
           </div>
-          {/* 
+        </div>
+        {/* 
       {[
         {
           id: 1,
@@ -119,61 +118,58 @@ export default function RoutePlace() {
           time: '6분',
         },
       ] */}
-          {routeArr.map((place: placeType, index: number) => (
-            <div
-              key={index + 1}
-              className="bg-white rounded-2xl overflow-hidden shadow-md hover:shadow-lg transition-all duration-300 cursor-pointer"
-            >
-              <div className="flex">
-                {/* <div className="w-28 h-28 bg-gradient-to-br from-indigo-100 to-purple-100"></div> */}
-                <div className="flex-1 p-4">
-                  <div className="flex items-start justify-between mb-2">
-                    <div>
-                      <div className="flex items-center gap-2 mb-1">
-                        <span className="text-sm font-semibold text-indigo-600">
-                          #{index + 1}
-                        </span>
-                        <h3 className="font-bold text-gray-900">
-                          {place.name}
-                        </h3>
-                      </div>
-                      {/* StatLabel 컴포넌트 사용 */}
-                      <StatLabel>{place.middleBizName}</StatLabel>
+        {routeArr.map((place: placeType, index: number) => (
+          <div
+            key={index + 1}
+            className="bg-white rounded-2xl overflow-hidden shadow-md hover:shadow-lg transition-all duration-300 cursor-pointer"
+          >
+            <div className="flex">
+              {/* <div className="w-28 h-28 bg-gradient-to-br from-indigo-100 to-purple-100"></div> */}
+              <div className="flex-1 p-4">
+                <div className="flex items-start justify-between mb-2">
+                  <div>
+                    <div className="flex items-center gap-2 mb-1">
+                      <span className="text-sm font-semibold text-indigo-600">
+                        #{index + 1}
+                      </span>
+                      <h3 className="font-bold text-gray-900">{place.name}</h3>
                     </div>
+                    {/* StatLabel 컴포넌트 사용 */}
+                    <StatLabel>{place.middleBizName}</StatLabel>
                   </div>
-                  <div className="grid items-center gap-3 text-sm text-gray-600">
-                    <div className="flex items-center gap-1">
-                      <Icon.Phone className="w-4 h-4 text-amber-400 fill-amber-400" />
-                      <span>{place.telNo}</span>
-                    </div>
-                    <div className="flex items-center gap-1">
-                      <Icon.MapPin className="w-4 h-4" />
-                      <span>
-                        {place.newAddressList.newAddress[0].fullAddressRoad}
-                      </span>
-                    </div>
-                    <div className="flex items-center gap-1">
-                      <Icon.Clock className="w-4 h-4" />
-                      <span>
-                        {getHourTimeMinTimeFormat(Number(place.radius)).hours >
-                        0
-                          ? `${getHourTimeMinTimeFormat(
-                              Number(place.radius)
-                            ).hours.toString()}시간 ${getHourTimeMinTimeFormat(
-                              Number(place.radius)
-                            ).minutes.toString()}분`
-                          : `${getHourTimeMinTimeFormat(
-                              Number(place.radius)
-                            ).minutes.toString()}분`}{' '}
-                      </span>
-                    </div>
+                </div>
+                <div className="grid items-center gap-3 text-sm text-gray-600">
+                  <div className="flex items-center gap-1">
+                    <Icon.Phone className="w-4 h-4 text-amber-400 fill-amber-400" />
+                    <span>{place.telNo}</span>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <Icon.MapPin className="w-4 h-4" />
+                    <span>
+                      {place.newAddressList.newAddress[0].fullAddressRoad}
+                    </span>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <Icon.Clock className="w-4 h-4" />
+                    <span>
+                      {getHourTimeMinTimeFormat(Number(place.radius)).hours > 0
+                        ? `${getHourTimeMinTimeFormat(
+                            Number(place.radius)
+                          ).hours.toString()}시간 ${getHourTimeMinTimeFormat(
+                            Number(place.radius)
+                          ).minutes.toString()}분`
+                        : `${getHourTimeMinTimeFormat(
+                            Number(place.radius)
+                          ).minutes.toString()}분`}{' '}
+                    </span>
                   </div>
                 </div>
               </div>
             </div>
-          ))}
-        </div>
-      )}
+          </div>
+        ))}
+      </div>
+      {/* )} */}
     </React.Fragment>
   )
 }
