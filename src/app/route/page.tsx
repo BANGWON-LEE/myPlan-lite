3301a4d8890b7fccf94b432c417a1b953838108f
BANGWON LEE <qkddnjs31@gmail.com>
2025-12-11 23:49:02 +0900

    add : dynamic import ssr false처리를 위한 작업

--- src/app/route/page.tsx
+++ src/app/route/page.tsx
@@ -1,15 +1,7 @@
 import RouteBottom from '@/features/Route/components/RouteBottom'
 import RouteHeader from '@/features/Route/components/RouteHeader'
 import RouteMain from '@/features/Route/components/RouteMain'
-// import RouteMap from '@/features/Route/components/RouteMap'
-import dynamic from 'next/dynamic'
-
-const RouteMap = dynamic(
-  () => import('../../features/Route/components/RouteMap'),
-  {
-    ssr: false,
-  }
-)
+import RouteMap from '@/features/Route/components/RouteMap'
 
 export default function RoutePage() {
   return (
---
+++ src/features/Route/components/RouteMapMain.tsx
@@ -0,0 +1,10 @@
+'use client'
+import dynamic from 'next/dynamic'
+
+const RouteMap = dynamic(() => import('../components/RouteMap'), {
+  ssr: false,
+})
+
+export default function RouteMapMain() {
+  return <RouteMap />
+}
