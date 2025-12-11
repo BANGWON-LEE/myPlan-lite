dcb3a7be295f410ea9addddaa09d78d70e0df7fd
BANGWON LEE <qkddnjs31@gmail.com>
2025-12-12 01:32:54 +0900

    edit : 로딩컴포넌트 재배치

--- src/features/Route/components/RouteCombined.tsx
+++ src/features/Route/components/RouteCombined.tsx
@@ -2,7 +2,7 @@
 
 import RoutePlace from './RoutePlace'
 // import RouteMap from './RouteMap'
-import { useLayoutEffect, useState } from 'react'
+import { Suspense, useLayoutEffect, useState } from 'react'
 import LoadingScreen from '@/features/loading/components/LoadingScreen'
 import dynamic from 'next/dynamic'
 const RouteMap = dynamic(() => import('../components/RouteMap'), {
@@ -10,29 +10,31 @@ const RouteMap = dynamic(() => import('../components/RouteMap'), {
 })
 
 export default function RouteCombined() {
-  const [loadingScreenAction, setLoadingSreenAction] = useState<boolean>(true)
+  // const [loadingScreenAction, setLoadingSreenAction] = useState<boolean>(true)
 
-  useLayoutEffect(() => {
-    const loadingTime = 1500
-    // setLoadingSreenAction(true)
+  // useLayoutEffect(() => {
+  //   const loadingTime = 1500
+  //   // setLoadingSreenAction(true)
 
-    const delayTime = setTimeout(() => {
-      setLoadingSreenAction(false)
-    }, loadingTime)
+  //   const delayTime = setTimeout(() => {
+  //     setLoadingSreenAction(false)
+  //   }, loadingTime)
 
-    return () => clearTimeout(delayTime)
-  }, [])
+  //   return () => clearTimeout(delayTime)
+  // }, [])
 
   return (
+    // <>
+    //   {loadingScreenAction ? (
+    //     <LoadingScreen />
+    //   ) : (
     <>
-      {loadingScreenAction ? (
-        <LoadingScreen />
-      ) : (
-        <>
-          <RouteMap />
-          <RoutePlace />
-        </>
-      )}
+      <RouteMap />
+      <Suspense fallback={<div></div>}>
+        <RoutePlace />
+      </Suspense>
     </>
+    //   )}
+    // </>
   )
 }
