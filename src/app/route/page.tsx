3eb8d4fe6d1e65e424f99883a3f29966344aef95
BANGWON LEE <qkddnjs31@gmail.com>
2025-12-11 23:02:16 +0900

    add : dynamic import

--- src/app/route/page.tsx
+++ src/app/route/page.tsx
@@ -1,7 +1,12 @@
 import RouteBottom from '@/features/Route/components/RouteBottom'
 import RouteHeader from '@/features/Route/components/RouteHeader'
 import RouteMain from '@/features/Route/components/RouteMain'
-import RouteMap from '@/features/Route/components/RouteMap'
+// import RouteMap from '@/features/Route/components/RouteMap'
+import dynamic from 'next/dynamic'
+
+const RouteMap = dynamic(
+  () => import('../../features/Route/components/RouteMap')
+)
 
 export default function RoutePage() {
   return (
