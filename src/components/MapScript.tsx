22c024f74a0098e2e99a60dfcdbd76668e511e1a
BANGWON LEE <qkddnjs31@gmail.com>
2025-12-17 21:24:41 +0900

    add : head에 preload 추가

--- src/components/MapScript.tsx
+++ src/components/MapScript.tsx
@@ -1,25 +1,34 @@
 'use client'
 
-import { onLoadMap } from '@/util/map/mapFunctions'
+import Head from 'next/head'
 import Script from 'next/script'
-import { useEffect, useLayoutEffect } from 'react'
 
 export default function MapScript() {
   return (
-    <Script
-      type="text/javascript"
-      src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID}&submodules=geocoder`}
-      // strategy="afterInteractive"
-      onLoad={() => {
-        new naver.maps.Map('map', {
-          center: new naver.maps.LatLng(
-            37.5665, // 서울
-            126.978
-          ),
-          zoom: 14,
-          mapTypeId: naver.maps.MapTypeId.NORMAL,
-        })
-      }}
-    ></Script>
+    <>
+      <Head>
+        <link
+          rel="preload"
+          as="script"
+          href={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID}&submodules=geocoder`}
+        />
+      </Head>
+
+      <Script
+        type="text/javascript"
+        src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID}&submodules=geocoder`}
+        // strategy="afterInteractive"
+        onLoad={() => {
+          new naver.maps.Map('map', {
+            center: new naver.maps.LatLng(
+              37.5665, // 서울
+              126.978
+            ),
+            zoom: 14,
+            mapTypeId: naver.maps.MapTypeId.NORMAL,
+          })
+        }}
+      ></Script>
+    </>
   )
 }
