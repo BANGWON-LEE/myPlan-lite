983a57c0064ff017b7ffe6b659e44baaa051e694
BANGWON LEE <qkddnjs31@gmail.com>
2026-02-17 17:01:38 +0900

    edit : pwa 적용을 위한 작업

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,8 @@ const withPWA = withPWAInit({
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  turbopack: {},
+  outputFileTracingRoot: process.cwd(),
 }
 
 export default withPWA(nextConfig)
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+import { GeistMono, GeistSans } from 'geist/font'
 
 export const metadata: Metadata = {
   title: '마이 플랜',
@@ -29,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}>
         <Analytics />
         {children}
       </body>
