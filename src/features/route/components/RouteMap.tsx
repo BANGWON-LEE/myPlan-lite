1c7d8d11b3302b526996dc03eaf47ad47616ace0
BANGWON LEE <qkddnjs31@gmail.com>
2026-03-28 15:36:59 +0900

    add : ref 사용하여 위치 관련 코드 작성

--- src/features/route/components/RouteMap.tsx
+++ src/features/route/components/RouteMap.tsx
@@ -1,20 +1,42 @@
 'use client'
 import MapScript from '@/components/MapScript'
+import {
+  startGpsWatch,
+  toGeolocationPosition,
+} from '@/features/route/containers/gpsMoveContainer'
 import { usePositionStore } from '@/stores/useRouteStore'
 import { MapScriptProps } from '@/types/placeType'
 import { savePositionToStorage } from '@/util/storage/positionStorage'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 export default function RouteMap(props: MapScriptProps) {
   const { position } = props
 
   const setPosition = usePositionStore(state => state.setPosition)
+  const stopGpsWatchRef = useRef<(() => void) | null>(null)
 
   useEffect(() => {
     if (!position) return
     setPosition(position)
     savePositionToStorage(position)
   }, [position, setPosition])
+
+  useEffect(() => {
+    stopGpsWatchRef.current?.()
+    stopGpsWatchRef.current = startGpsWatch({
+      onCoordinateUpdate: (coordinate, timestamp) => {
+        const nextPosition = toGeolocationPosition(coordinate, timestamp)
+        setPosition(nextPosition)
+        savePositionToStorage(nextPosition)
+      },
+    })
+
+    return () => {
+      stopGpsWatchRef.current?.()
+      stopGpsWatchRef.current = null
+    }
+  }, [setPosition])
+
   return (
     <>
       <div className="relative h-64 bg-gradient-to-br from-blue-100 to-indigo-100">
@@ -24,6 +46,10 @@ export default function RouteMap(props: MapScriptProps) {
         </div>
         {/* 루트 정보 카드 - StatValue & StatLabel 컴포넌트 사용 */}
       </div>
+      <div
+        id="gps-status-message"
+        className="min-h-5 px-1 pt-2 text-xs text-amber-700"
+      ></div>
     </>
   )
 }
