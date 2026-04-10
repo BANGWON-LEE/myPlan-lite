f9af6816cdd1334de6f4f700e2f80d07d090b938
BANGWON LEE <qkddnjs31@gmail.com>
2026-04-11 01:01:10 +0900

    edit : mapRef를 전역적으로 관리함으로 코드 추가

--- src/components/MapScript.tsx
+++ src/components/MapScript.tsx
@@ -1,47 +1,46 @@
 'use client'
 
-import { usePositionStore } from '@/stores/useRouteStore'
-import { DEFAULT_MAP_ZOOM, onLoadRouteMap } from '@/util/map/mapFunctions'
+import { useMapStore, usePositionStore } from '@/stores/useRouteStore'
+import { DEFAULT_MAP_ZOOM } from '@/util/map/mapFunctions'
 import Script from 'next/script'
 import { useEffect } from 'react'
 
 export default function MapScript() {
   const position = usePositionStore(state => state.position)
+  const setMap = useMapStore(state => state.setMap)
 
   function handleMapLoad() {
     try {
       const pos = usePositionStore.getState().position
-      onLoadRouteMap(
-        new naver.maps.Map('map', {
-          center: new naver.maps.LatLng(
-            pos?.coords.latitude ?? 37.5665,
-            pos?.coords.longitude ?? 126.978,
-          ),
-          zoom: DEFAULT_MAP_ZOOM,
-          mapTypeId: naver.maps.MapTypeId.NORMAL,
-        }),
-      )
+      const map = new naver.maps.Map('map', {
+        center: new naver.maps.LatLng(
+          pos?.coords.latitude ?? 37.5665,
+          pos?.coords.longitude ?? 126.978,
+        ),
+        zoom: DEFAULT_MAP_ZOOM,
+        mapTypeId: naver.maps.MapTypeId.NORMAL,
+      })
+      setMap(map)
     } catch (error) {
       // 전역으로 가져오는 좌표값에 문제가 생길 때, localStorage에서 좌표값을 가져와 fallback으로 사용한다.
       console.error('Error loading Naver Map:', error)
       const pos = localStorage.getItem('position')
-      onLoadRouteMap(
-        new naver.maps.Map('map', {
-          center: new naver.maps.LatLng(
-            pos ? JSON.parse(pos).coords.latitude : 37.5665,
-            pos ? JSON.parse(pos).coords.longitude : 126.978,
-          ),
-          zoom: DEFAULT_MAP_ZOOM,
-          mapTypeId: naver.maps.MapTypeId.NORMAL,
-        }),
-      )
+      const map = new naver.maps.Map('map', {
+        center: new naver.maps.LatLng(
+          pos ? JSON.parse(pos).coords.latitude : 37.5665,
+          pos ? JSON.parse(pos).coords.longitude : 126.978,
+        ),
+        zoom: DEFAULT_MAP_ZOOM,
+        mapTypeId: naver.maps.MapTypeId.NORMAL,
+      })
+      setMap(map)
     }
   }
 
   useEffect(() => {
     if (typeof naver === 'undefined') return
     handleMapLoad()
-  }, [position])
+  }, [position, setMap])
 
   return (
     <>
