2ca3000d247769eda53bfda44fc48c33eab748b5
BANGWON LEE <qkddnjs31@gmail.com>
2026-07-17 11:19:08 +0900

    feat : 현재 위치 조회 fallback 반영

--- src/util/map/query.ts
+++ src/util/map/query.ts
@@ -1,14 +1,18 @@
 import { POSITION_QUERY_KEY } from '@/lib/queryKeys'
 import { useQuery } from '@tanstack/react-query'
-import { getCurrentPositionPromise } from './mapFunctions'
+import {
+  getCurrentPositionWithFallback,
+  SmartLocationResult,
+} from '../location/location'
 
 export function useCurrentPositionQuery() {
   return useQuery<
-    GeolocationPosition,
+    // GeolocationPosition,
+    SmartLocationResult,
     globalThis.Error | GeolocationPositionError
   >({
     queryKey: POSITION_QUERY_KEY,
-    queryFn: async () => await getCurrentPositionPromise(),
+    queryFn: async () => await getCurrentPositionWithFallback(),
     staleTime: 1000 * 60 * 5,
     retry: false,
   })
